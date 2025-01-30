2ff8d54d25ca0bb8802372ddfc3b552e814502de
Shivaram Karthikeyan <shivaramkpk@gmail.com>
2025-01-30 20:26:06 +0530

    port change

--- terraform/ec2inst/secgrp.tf
+++ terraform/ec2inst/secgrp.tf
@@ -35,3 +35,19 @@ resource "aws_vpc_security_group_egress_rule" "allowalloutbound_ipv6" {
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_app1_port" {
+  security_group_id = aws_security_group.ec2.id
+  cidr_ipv4         = "0.0.0.0/0"  # Allow from anywhere (change to restrict access)
+  from_port         = 8081
+  ip_protocol       = "tcp"
+  to_port           = 8081
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_app2_port" {
+  security_group_id = aws_security_group.ec2.id
+  cidr_ipv4         = "0.0.0.0/0"  # Allow from anywhere (change to restrict access)
+  from_port         = 8082
+  ip_protocol       = "tcp"
+  to_port           = 8082
+}
